dd4813322d4544649fb2fc2b6494327f80a35fd9
lampo1024 <lampo1024@gmail.com>
2019-09-21 14:06:21 +0800

    Add product application service

---
+++ src/DddCoreExample.Application/Products/IProductService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DddCoreExample.Application.Products
+{
+    public interface IProductService
+    {
+        ProductDto Get(Guid productId);
+        ProductDto Add(ProductDto product);
+    }
+}
---
+++ src/DddCoreExample.Application/Products/ProductService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using AutoMapper;
+using DddCoreExample.Domain.Models.Products;
+using DddCoreExample.Domain.Repository;
+
+namespace DddCoreExample.Application.Products
+{
+    public class ProductService : IProductService
+    {
+        private readonly IRepository<Product> _productRepository;
+        private readonly IRepository<ProductCode> _productCodeRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public ProductService(IRepository<Product> productRepository, IRepository<ProductCode> productCodeRepository, IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _productRepository = productRepository;
+            _productCodeRepository = productCodeRepository;
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public ProductDto Get(Guid productId)
+        {
+            var product = _productRepository.FindById(productId);
+            return _mapper.Map<Product, ProductDto>(product);
+        }
+
+        public ProductDto Add(ProductDto productDto)
+        {
+            var productCode = _productCodeRepository.FindById(productDto.ProductCodeId);
+            if (productCode == null)
+            {
+                throw new Exception("Product Code Is Not Valid");
+            }
+
+            var product = Product.Create(productDto.Name, productDto.Quantity, productDto.Cost, productCode);
+
+            _productRepository.Add(product);
+            return _mapper.Map<Product, ProductDto>(product);
+        }
+
+    }
+}
